bb22d2e9ee9c6088adb11691b1fbd2fb565980a6
김승완 <wani001110@naver.com>
2025-10-31 21:25:48 +0900

    feat: 제목(title) 30자 초과 검증 추가

--- src/main/java/edu/causwict/restapi/service/PostService.java
+++ src/main/java/edu/causwict/restapi/service/PostService.java
@@ -23,6 +23,11 @@ public class PostService {
         }
         // trim()을 통해서 ""인경우와 "    " 처럼 여러번 공백들어간 경우 모두 ""로 바꿔버림.
 
+        // 제목 30글자 넘는지 체크
+        if (title.length() > 30) {
+            throw new IllegalArgumentException("제목(title)은 30자를 넘을 수 없습니다.");
+        }
+
 		Post post = new Post(null, title, content);
 		return postRepository.save(post);
 	}
@@ -31,6 +36,11 @@ public class PostService {
         // 제목 비어있는지 검증하는 로직 추가, create와 update 메소드 전부 추가
         if (title == null || title.trim().isEmpty()) {
             throw new IllegalArgumentException("제목(title)은 비어 있을 수 없습니다.");
+        }
+
+        // 제목 30글자 넘는지 체크
+        if (title.length() > 30) {
+            throw new IllegalArgumentException("제목(title)은 30자를 넘을 수 없습니다.");
         }
 
         // 1. 기존 게시글이 존재하는지 확인
